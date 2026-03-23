9e04de54e8c103eb86b2bfaede97e2c027d0b619
Alekzio <a31410591@gmail.com>
2026-03-23 09:38:28 +0100

    update

--- sources/main.py
+++ sources/main.py
@@ -55,7 +55,7 @@ FLAGS = {
     6:  {"flag": "28473619", "points": 150, "redirect": "/level4"},
     # Level 4 sous-énigmes
     41: {"flag": "4301",     "points": 100, "redirect": "/level4"},
-    42: {"flag": "2217",     "points": 100, "redirect": "/level4"},
+    42: {"flag": "3318",     "points": 100, "redirect": "/level4"},
     43: {"flag": "1974",     "points": 100, "redirect": "/level4"},
     # Level 4 code final
     44: {"flag": "430119",   "points": 300, "redirect": "/level5"},
